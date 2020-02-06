9f97be73b6d1cc210dd25b1765ab870e1f20b95d
Samia <34365874+Samia1743@users.noreply.github.com>
2020-02-06 20:52:03 +0600

    changing the condition

--- basicwhile.py
+++ basicwhile.py
@@ -1,9 +1,10 @@
 condition=1
 while condition <10:
+    condition+=1
+    condition+=1
     print(condition)
-    condition +=1
+    condition+=1
 while True:
     print('doing stuff')
     break
-    
 
