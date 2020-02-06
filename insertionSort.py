de815fc1d61b87e109bef0a0f0043ab40d22dda1
Samia <34365874+Samia1743@users.noreply.github.com>
2020-02-06 22:20:45 +0600

    insertion algorithm code

---
+++ insertionSort.py
@@ -0,0 +1,16 @@
+def insertionSort(a):
+    for i in range(1,len(a)):
+        key = a[i]
+        j=i-1
+        while j>=0 and key<a[j]:
+            
+            a[j+1]=a[j]
+            j=j-1
+        a[j+1]=key
+a=[12,11,13,5,6]
+print(a)
+insertionSort(a)
+print(a)
+#for i in range (len(a)):
+    
+    #print(a[i])
