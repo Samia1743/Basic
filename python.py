9b1ae66a53d11dd00c982e3caad7aa0d2771f1fe
Samia <34365874+Samia1743@users.noreply.github.com>
2020-02-06 22:22:26 +0600

    new basic python code

---
+++ python.py
@@ -0,0 +1,20 @@
+a=[5,4,4,3,1,-2,-3,-5,-9,-7]
+total3=0
+i=len(a)-1
+while a[i]<0:
+    total3 +=a[i]
+    i=i-1
+print(total3)
+b=[20,10,5]
+total=0
+for e in b:
+    total=total+e
+print(total)
+
+c=["apple",'banana','microsoft' ]
+for element in c:
+    print(element)
+    print(element)
+
+
+    
